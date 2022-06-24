7880d1fc2e9ecfd1f6b252b9f97ace2b40b9ea85
Jim Ezesinachi <ezesinachijim@gmail.com>
2022-06-24 17:00:42 +0100

    feat: added image component and useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
-import Form from './components/Form/Form';
-import Meme from './components/Meme/Meme';
+import Main from './components/Main/Main';
 
 export default function App () {
   return (
     <div className="App">
       <Header />
-      <Form />
-      <Meme />
+      <Main />
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,37 @@
 import './Main.css';
-import Form from '../Form/Form';
-import Meme from '../Meme/Meme';
+import React from 'react';
+import memesData from '../../data/memesData';
 
-export default function Main () {
+export default function Form () {
+    const allMemeImages = memesData.data.memes;
+    let meme = allMemeImages[Math.floor(Math.random() * allMemeImages.length)];
+    meme = {
+        topText: '',
+        bottomText: '',
+        randomImage: meme.url,
+        name: meme.name
+    }
+
+    const [memeImage, setMemeImage] = React.useState(meme);
+    
+    function changeMemeImage () {
+        meme = allMemeImages[Math.floor(Math.random() * allMemeImages.length)];
+        meme = {
+            ...meme,
+            randomImage: meme.url,
+            name: meme.name
+        }
+        setMemeImage(meme);
+    }
+
     return (
         <main className='main'>
-            <Form />
-            <Meme />
+            <div className='main--input'>
+                <input type='text' placeholder='Enter setup line here' className='main--input-setup'/>
+                <input type='text' placeholder='Enter punchline here' className='main--input-punchline'/>
+            </div>
+            <button onClick={changeMemeImage} className='main--submit'>Get a new meme image 🖼</button>
+            <img src={memeImage.randomImage} alt={memeImage.name} className='main--meme-image' />
         </main>
     )
 }
